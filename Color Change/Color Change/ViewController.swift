48fedf914534f52100193842a39038173e46b77a
wkpgro <xr@192.168.10.131>
2018-04-30 02:21:22 -0700

    Initialize

---
+++ Color Change/Color Change/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Color Change
+//
+//  Created by xr on 4/30/18.
+//  Copyright © 2018 Josh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
